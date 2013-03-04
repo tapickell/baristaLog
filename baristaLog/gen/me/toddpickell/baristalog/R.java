55792494e3296a579e6e12671eab8e8ba501dc98
myappleguy <myappleguy@gmail.com>
2013-03-03 18:06:28 -0600

    added labels for timers and state changes by dropdpwn selection

--- baristaLog/gen/me/toddpickell/baristalog/R.java
+++ baristaLog/gen/me/toddpickell/baristalog/R.java
@@ -18,35 +18,45 @@ public final class R {
         public static final int textview_border=0x7f020001;
     }
     public static final class id {
-        public static final int addressTextView=0x7f080009;
-        public static final int bloom_text_view=0x7f080002;
-        public static final int brew_text_view=0x7f080003;
-        public static final int cityEditText=0x7f08000b;
-        public static final int cityLabelTextView=0x7f08001a;
-        public static final int cityTableRow=0x7f080019;
-        public static final int cityTextView=0x7f08001b;
+        public static final int addressTextView=0x7f080013;
+        public static final int bloom_text_timer=0x7f080007;
+        public static final int bloom_text_view=0x7f080003;
+        public static final int brew_text_timer=0x7f080008;
+        public static final int brew_text_view=0x7f080004;
+        public static final int cityEditText=0x7f080015;
+        public static final int cityLabelTextView=0x7f080024;
+        public static final int cityTableRow=0x7f080023;
+        public static final int cityTextView=0x7f080025;
         public static final int devices_spinner=0x7f080000;
-        public static final int emailEditText=0x7f080008;
-        public static final int emailLabelTextView=0x7f080014;
-        public static final int emailTableRow=0x7f080013;
-        public static final int emailTextView=0x7f080015;
-        public static final int linearLayout=0x7f080005;
-        public static final int menu_settings=0x7f08001c;
-        public static final int nameEditText=0x7f080006;
-        public static final int nameLabelTextView=0x7f08000e;
-        public static final int nameTableRow=0x7f08000d;
-        public static final int nameTextView=0x7f08000f;
-        public static final int phoneEditText=0x7f080007;
-        public static final int phoneLabelTextView=0x7f080011;
-        public static final int phoneTableRow=0x7f080010;
-        public static final int phoneTextView=0x7f080012;
-        public static final int pre_text_view=0x7f080001;
-        public static final int saveContactButton=0x7f08000c;
-        public static final int streetEditText=0x7f08000a;
-        public static final int streetLabelTextView=0x7f080017;
-        public static final int streetTableRow=0x7f080016;
-        public static final int streetTextView=0x7f080018;
-        public static final int textView1=0x7f080004;
+        public static final int emailEditText=0x7f080012;
+        public static final int emailLabelTextView=0x7f08001e;
+        public static final int emailTableRow=0x7f08001d;
+        public static final int emailTextView=0x7f08001f;
+        public static final int linearLayout=0x7f08000f;
+        public static final int menu_settings=0x7f080026;
+        public static final int nameEditText=0x7f080010;
+        public static final int nameLabelTextView=0x7f080018;
+        public static final int nameTableRow=0x7f080017;
+        public static final int nameTextView=0x7f080019;
+        public static final int phoneEditText=0x7f080011;
+        public static final int phoneLabelTextView=0x7f08001b;
+        public static final int phoneTableRow=0x7f08001a;
+        public static final int phoneTextView=0x7f08001c;
+        public static final int pre_text_timer=0x7f080006;
+        public static final int pre_text_view=0x7f080002;
+        public static final int saveContactButton=0x7f080016;
+        public static final int streetEditText=0x7f080014;
+        public static final int streetLabelTextView=0x7f080021;
+        public static final int streetTableRow=0x7f080020;
+        public static final int streetTextView=0x7f080022;
+        public static final int sub_text_timer=0x7f08000b;
+        public static final int sub_text_view=0x7f08000a;
+        public static final int sub_timer_container=0x7f080009;
+        public static final int time_label_container=0x7f080001;
+        public static final int time_timer_container=0x7f080005;
+        public static final int total_text_timer=0x7f08000e;
+        public static final int total_text_view=0x7f08000d;
+        public static final int total_timer_container=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,7 +69,9 @@ public final class R {
     public static final class string {
         public static final int address=0x7f040008;
         public static final int app_name=0x7f040000;
+        public static final int bloom_text_timer=0x7f040016;
         public static final int bloom_text_view=0x7f040011;
+        public static final int brew_text_timer=0x7f040017;
         public static final int brew_text_view=0x7f040012;
         public static final int button_save_contact=0x7f04000e;
         public static final int device_spinner=0x7f04000f;
@@ -76,8 +88,13 @@ public final class R {
         public static final int label_street=0x7f040006;
         public static final int menu_settings=0x7f040002;
         public static final int plunge_text_view=0x7f040014;
+        public static final int pre_text_timer=0x7f040015;
         public static final int pre_text_view=0x7f040010;
         public static final int steep_text_view=0x7f040013;
+        public static final int sub_text_timer=0x7f040019;
+        public static final int sub_text_view=0x7f040018;
+        public static final int total_text_timer=0x7f04001b;
+        public static final int total_text_view=0x7f04001a;
     }
     public static final class style {
         /** 
--- baristaLog/src/me/toddpickell/baristalog/MainActivity.java
+++ baristaLog/src/me/toddpickell/baristalog/MainActivity.java
@@ -4,6 +4,7 @@ package me.toddpickell.baristalog;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,20 +16,32 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements OnItemSelectedListener {
 	
 	private Spinner spinner;
-	private TextView textView1;
 	private TextView pre_text_view;
 	private TextView bloom_text_view;
 	private TextView brew_text_view;
+	private TextView pre_text_timer;
+	private TextView bloom_text_timer;
+	private TextView brew_text_timer;
+	private TextView sub_text_view;
+	private TextView sub_text_timer;
+	private TextView total_text_view;
+	private TextView total_text_timer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		textView1 = (TextView) findViewById(R.id.textView1);
 		pre_text_view = (TextView) findViewById(R.id.pre_text_view);
 		bloom_text_view = (TextView) findViewById(R.id.bloom_text_view);
 		brew_text_view = (TextView) findViewById(R.id.brew_text_view);
+		pre_text_timer = (TextView) findViewById(R.id.pre_text_timer);
+		bloom_text_timer = (TextView) findViewById(R.id.bloom_text_timer);
+		brew_text_timer = (TextView) findViewById(R.id.brew_text_timer);
+		sub_text_view = (TextView) findViewById(R.id.sub_text_view);
+		sub_text_timer = (TextView) findViewById(R.id.sub_text_timer);
+		total_text_view = (TextView) findViewById(R.id.total_text_view);
+		total_text_timer = (TextView) findViewById(R.id.total_text_timer);
 		
 		//setup array adapter for spinner
 		spinner = (Spinner) findViewById(R.id.devices_spinner);
@@ -86,46 +99,90 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 		
 	}
 	
-	private void setTextFieldNormal() {
-		pre_text_view.setText(R.string.pre_text_view);
-		bloom_text_view.setText(R.string.bloom_text_view);
-		brew_text_view.setText(R.string.brew_text_view);
+	//will pull times from preferences file later
+	private void setTimesForDeviceState(Integer pre, Integer bloom, Integer brew) {
+		pre_text_timer.setText(DateUtils.formatElapsedTime(pre));
+		bloom_text_timer.setText(DateUtils.formatElapsedTime(bloom));
+		brew_text_timer.setText(DateUtils.formatElapsedTime(brew));
+		sub_text_timer.setText(DateUtils.formatElapsedTime(pre));
+		total_text_timer.setText(DateUtils.formatElapsedTime(pre + bloom + brew));
 	}
 	
 	private void setStateForAeropress() {
-		textView1.setText("Aeropress");
+		Integer pre = 10;
+		Integer bloom = 20;
 		pre_text_view.setText(R.string.steep_text_view);
 		bloom_text_view.setText(R.string.plunge_text_view);
-		brew_text_view.setText("");		
+		brew_text_view.setText("");
+		sub_text_view.setText(R.string.steep_text_view);
+		total_text_view.setText("Total");
+		pre_text_timer.setText(DateUtils.formatElapsedTime(pre));
+		bloom_text_timer.setText(DateUtils.formatElapsedTime(bloom));
+		brew_text_timer.setText("");
+		sub_text_timer.setText(DateUtils.formatElapsedTime(pre));
+		total_text_timer.setText(DateUtils.formatElapsedTime((pre + bloom)));
+		
 	}
 	
 	private void setStateForChemex() {
-		textView1.setText("Chemex");
-		setTextFieldNormal();
+		Integer pre = 30;
+		Integer bloom = 30;
+		Integer brew = 180;
+		pre_text_view.setText(R.string.pre_text_view);
+		bloom_text_view.setText(R.string.bloom_text_view);
+		brew_text_view.setText(R.string.brew_text_view);
+		sub_text_view.setText(R.string.pre_text_view);
+		total_text_view.setText("Total");
+		setTimesForDeviceState(pre, bloom, brew);
 	}
 	
 	private void setStateForClever() {
-		textView1.setText("Clever");
-		setTextFieldNormal();
+		Integer pre = 30;
+		Integer bloom = 30;
+		Integer brew = 180;
+		pre_text_view.setText(R.string.pre_text_view);
+		bloom_text_view.setText(R.string.bloom_text_view);
+		brew_text_view.setText(R.string.brew_text_view);
+		sub_text_view.setText(R.string.pre_text_view);
+		total_text_view.setText("Total");
+		setTimesForDeviceState(pre, bloom, brew);
 	}
 	
 	private void setStateForEspresso() {
-		textView1.setText("Espresso");
 		pre_text_view.setText("");
 		bloom_text_view.setText("");
 		brew_text_view.setText("");
+		sub_text_view.setText("");
+		total_text_view.setText("Total");
+		pre_text_timer.setText("");
+		bloom_text_timer.setText("");
+		brew_text_timer.setText("");
+		sub_text_timer.setText("");
+		total_text_timer.setText(DateUtils.formatElapsedTime(0));
 	}
 	
 	private void setStateForFrenchPress() {
-		textView1.setText("French Press");
+		Integer pre = 30;
+		Integer bloom = 180;
+		Integer brew = 30;
 		pre_text_view.setText(R.string.bloom_text_view);
 		bloom_text_view.setText(R.string.brew_text_view);
 		brew_text_view.setText(R.string.plunge_text_view);
+		sub_text_view.setText(R.string.bloom_text_view);
+		total_text_view.setText("Total");
+		setTimesForDeviceState(pre, bloom, brew);
 	}
 	
 	private void setStateForPourOver() {
-		textView1.setText("Pour Over");
-		setTextFieldNormal();
+		Integer pre = 30;
+		Integer bloom = 30;
+		Integer brew = 90;
+		pre_text_view.setText(R.string.pre_text_view);
+		bloom_text_view.setText(R.string.bloom_text_view);
+		brew_text_view.setText(R.string.brew_text_view);
+		sub_text_view.setText(R.string.pre_text_view);
+		total_text_view.setText("Total");
+		setTimesForDeviceState(pre, bloom, brew);
 	}
 
 }
