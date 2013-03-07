5b8b490ad9e69e1c61455bd1ea84ac550a62133c
myappleguy <myappleguy@gmail.com>
2013-03-07 12:49:44 -0600

    got sounds added for button press, sub time ding and total time ding

--- baristaLog/gen/me/toddpickell/baristalog/R.java
+++ baristaLog/gen/me/toddpickell/baristalog/R.java
@@ -9,61 +9,61 @@ package me.toddpickell.baristalog;
 
 public final class R {
     public static final class array {
-        public static final int devices_array=0x7f060000;
+        public static final int devices_array=0x7f070000;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int GREEN=0x7f040000;
-        public static final int RED=0x7f040001;
-        public static final int WHITE=0x7f040002;
+        public static final int GREEN=0x7f050000;
+        public static final int RED=0x7f050001;
+        public static final int WHITE=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int textview_border=0x7f020001;
     }
     public static final class id {
-        public static final int addressTextView=0x7f090015;
-        public static final int bloom_text_timer=0x7f090007;
-        public static final int bloom_text_view=0x7f090003;
-        public static final int brew_text_timer=0x7f090008;
-        public static final int brew_text_view=0x7f090004;
-        public static final int cityEditText=0x7f090017;
-        public static final int cityLabelTextView=0x7f090026;
-        public static final int cityTableRow=0x7f090025;
-        public static final int cityTextView=0x7f090027;
-        public static final int devices_spinner=0x7f090000;
-        public static final int emailEditText=0x7f090014;
-        public static final int emailLabelTextView=0x7f090020;
-        public static final int emailTableRow=0x7f09001f;
-        public static final int emailTextView=0x7f090021;
-        public static final int linearLayout=0x7f090011;
-        public static final int menu_settings=0x7f090028;
-        public static final int nameEditText=0x7f090012;
-        public static final int nameLabelTextView=0x7f09001a;
-        public static final int nameTableRow=0x7f090019;
-        public static final int nameTextView=0x7f09001b;
-        public static final int phoneEditText=0x7f090013;
-        public static final int phoneLabelTextView=0x7f09001d;
-        public static final int phoneTableRow=0x7f09001c;
-        public static final int phoneTextView=0x7f09001e;
-        public static final int pre_text_timer=0x7f090006;
-        public static final int pre_text_view=0x7f090002;
-        public static final int saveContactButton=0x7f090018;
-        public static final int start_stop_button=0x7f090010;
-        public static final int start_stop_button_container=0x7f09000f;
-        public static final int streetEditText=0x7f090016;
-        public static final int streetLabelTextView=0x7f090023;
-        public static final int streetTableRow=0x7f090022;
-        public static final int streetTextView=0x7f090024;
-        public static final int sub_text_timer=0x7f09000b;
-        public static final int sub_text_view=0x7f09000a;
-        public static final int sub_timer_container=0x7f090009;
-        public static final int time_label_container=0x7f090001;
-        public static final int time_timer_container=0x7f090005;
-        public static final int total_text_timer=0x7f09000e;
-        public static final int total_text_view=0x7f09000d;
-        public static final int total_timer_container=0x7f09000c;
+        public static final int addressTextView=0x7f0a0015;
+        public static final int bloom_text_timer=0x7f0a0007;
+        public static final int bloom_text_view=0x7f0a0003;
+        public static final int brew_text_timer=0x7f0a0008;
+        public static final int brew_text_view=0x7f0a0004;
+        public static final int cityEditText=0x7f0a0017;
+        public static final int cityLabelTextView=0x7f0a0026;
+        public static final int cityTableRow=0x7f0a0025;
+        public static final int cityTextView=0x7f0a0027;
+        public static final int devices_spinner=0x7f0a0000;
+        public static final int emailEditText=0x7f0a0014;
+        public static final int emailLabelTextView=0x7f0a0020;
+        public static final int emailTableRow=0x7f0a001f;
+        public static final int emailTextView=0x7f0a0021;
+        public static final int linearLayout=0x7f0a0011;
+        public static final int menu_settings=0x7f0a0028;
+        public static final int nameEditText=0x7f0a0012;
+        public static final int nameLabelTextView=0x7f0a001a;
+        public static final int nameTableRow=0x7f0a0019;
+        public static final int nameTextView=0x7f0a001b;
+        public static final int phoneEditText=0x7f0a0013;
+        public static final int phoneLabelTextView=0x7f0a001d;
+        public static final int phoneTableRow=0x7f0a001c;
+        public static final int phoneTextView=0x7f0a001e;
+        public static final int pre_text_timer=0x7f0a0006;
+        public static final int pre_text_view=0x7f0a0002;
+        public static final int saveContactButton=0x7f0a0018;
+        public static final int start_stop_button=0x7f0a0010;
+        public static final int start_stop_button_container=0x7f0a000f;
+        public static final int streetEditText=0x7f0a0016;
+        public static final int streetLabelTextView=0x7f0a0023;
+        public static final int streetTableRow=0x7f0a0022;
+        public static final int streetTextView=0x7f0a0024;
+        public static final int sub_text_timer=0x7f0a000b;
+        public static final int sub_text_view=0x7f0a000a;
+        public static final int sub_timer_container=0x7f0a0009;
+        public static final int time_label_container=0x7f0a0001;
+        public static final int time_timer_container=0x7f0a0005;
+        public static final int total_text_timer=0x7f0a000e;
+        public static final int total_text_view=0x7f0a000d;
+        public static final int total_timer_container=0x7f0a000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -71,37 +71,42 @@ public final class R {
         public static final int view_contact=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
+        public static final int activity_main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int click=0x7f040000;
+        public static final int ding=0x7f040001;
+        public static final int tick=0x7f040002;
     }
     public static final class string {
-        public static final int address=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int bloom_text_timer=0x7f050016;
-        public static final int bloom_text_view=0x7f050011;
-        public static final int brew_text_timer=0x7f050017;
-        public static final int brew_text_view=0x7f050012;
-        public static final int button_save_contact=0x7f05000e;
-        public static final int device_spinner=0x7f05000f;
-        public static final int hello_world=0x7f050001;
-        public static final int hint_city=0x7f05000d;
-        public static final int hint_email=0x7f05000b;
-        public static final int hint_name=0x7f050009;
-        public static final int hint_phone=0x7f05000a;
-        public static final int hint_street=0x7f05000c;
-        public static final int label_city=0x7f050007;
-        public static final int label_email=0x7f050005;
-        public static final int label_name=0x7f050003;
-        public static final int label_phone=0x7f050004;
-        public static final int label_street=0x7f050006;
-        public static final int menu_settings=0x7f050002;
-        public static final int plunge_text_view=0x7f050014;
-        public static final int pre_text_timer=0x7f050015;
-        public static final int pre_text_view=0x7f050010;
-        public static final int steep_text_view=0x7f050013;
-        public static final int sub_text_timer=0x7f050019;
-        public static final int sub_text_view=0x7f050018;
-        public static final int total_text_timer=0x7f05001b;
-        public static final int total_text_view=0x7f05001a;
+        public static final int address=0x7f060008;
+        public static final int app_name=0x7f060000;
+        public static final int bloom_text_timer=0x7f060016;
+        public static final int bloom_text_view=0x7f060011;
+        public static final int brew_text_timer=0x7f060017;
+        public static final int brew_text_view=0x7f060012;
+        public static final int button_save_contact=0x7f06000e;
+        public static final int device_spinner=0x7f06000f;
+        public static final int hello_world=0x7f060001;
+        public static final int hint_city=0x7f06000d;
+        public static final int hint_email=0x7f06000b;
+        public static final int hint_name=0x7f060009;
+        public static final int hint_phone=0x7f06000a;
+        public static final int hint_street=0x7f06000c;
+        public static final int label_city=0x7f060007;
+        public static final int label_email=0x7f060005;
+        public static final int label_name=0x7f060003;
+        public static final int label_phone=0x7f060004;
+        public static final int label_street=0x7f060006;
+        public static final int menu_settings=0x7f060002;
+        public static final int plunge_text_view=0x7f060014;
+        public static final int pre_text_timer=0x7f060015;
+        public static final int pre_text_view=0x7f060010;
+        public static final int steep_text_view=0x7f060013;
+        public static final int sub_text_timer=0x7f060019;
+        public static final int sub_text_view=0x7f060018;
+        public static final int total_text_timer=0x7f06001b;
+        public static final int total_text_view=0x7f06001a;
     }
     public static final class style {
         /** 
@@ -125,12 +130,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int LogLabelTextView=0x7f070002;
-        public static final int LogTextView=0x7f070003;
+        public static final int AppTheme=0x7f080001;
+        public static final int LogLabelTextView=0x7f080002;
+        public static final int LogTextView=0x7f080003;
     }
 }
--- baristaLog/src/me/toddpickell/baristalog/MainActivity.java
+++ baristaLog/src/me/toddpickell/baristalog/MainActivity.java
@@ -1,14 +1,17 @@
 package me.toddpickell.baristalog;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,6 +24,10 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnItemSelectedListener, OnClickListener {
 	
+	private static final Integer TICK_SOUND_ID = 0;
+	private static final Integer DING_SOUND_ID = 1;
+	private static final Integer CLICK_SOUND_ID = 2;
+
 	private Spinner spinner;
 	
 	private TextView sub_text_view;
@@ -33,19 +40,27 @@ public class MainActivity extends Activity implements OnItemSelectedListener, On
 	private List<Integer> subTimes;
 	private List<String> deviceNames;
 	
-	private Integer total;
 	private long mStart = 0;
     private long mTotalTime = 0;
     private long mSubTime = 0;
     
-    private Boolean countdown;
-    
     private DeviceState device;
+    
+    private SoundPool soundPool;
+	private HashMap<Integer, Integer> soundMap;
 
+	@SuppressLint("UseSparseArrays")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		soundMap = new HashMap<Integer, Integer>(); 
+		soundMap.put(TICK_SOUND_ID, soundPool.load(this, R.raw.tick, 1));
+		soundMap.put(DING_SOUND_ID, soundPool.load(this, R.raw.ding, 1));
+		soundMap.put(CLICK_SOUND_ID, soundPool.load(this, R.raw.click, 1));
 		
 		sub_text_view = (TextView) findViewById(R.id.sub_text_view);
 		sub_text_timer = (TextView) findViewById(R.id.sub_text_timer);
@@ -83,6 +98,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener, On
 		} else {
 			stopTimer();
 		}
+		soundPool.play(soundMap.get(CLICK_SOUND_ID), 1, 1, 1, 0, 1f);
 	}
 	
 	private void stopTimer() {
@@ -166,22 +182,25 @@ public class MainActivity extends Activity implements OnItemSelectedListener, On
 			}
             mStart = current;
 //            Log.d("DEBUGGERY", "Current: " + current/1000 + " TotalTime: " + mTotalTime + " mStart: " + mStart/1000 + " Total: " + ((mTotalTime/1000) + total));
-            Log.d("DEBUGGERY", "mSubTime: " + ((mTotalTime/1000) + mSubTime));
+//            Log.d("DEBUGGERY", "mSubTime: " + ((mTotalTime/1000) + mSubTime));
 
             if (((mTotalTime/1000) + mSubTime) <= 0) {
+            	//play tick.ogg
+            	soundPool.play(soundMap.get(TICK_SOUND_ID), 1, 1, 1, 0, 1f);
+            	
 				//change sub title and time if there is one
             	if (!subTitles.isEmpty()) {
 					sub_text_view.setText(subTitles.get(0));
 					subTitles.remove(0);
 					if (!subTimes.isEmpty()) {
-						Log.d("DEBUGGERY", "mSubTime before change:" + mSubTime);
+//						Log.d("DEBUGGERY", "mSubTime before change:" + mSubTime);
 						mSubTime = subTimes.get(0);
 						subTimes.remove(0);
-						Log.d("DEBUGGERY", "mSubTime after change:" + mSubTime);
-						Log.d("DEGUGGERY", "mTotalTime/1000: " + (mTotalTime/1000));
+//						Log.d("DEBUGGERY", "mSubTime after change:" + mSubTime);
+//						Log.d("DEGUGGERY", "mTotalTime/1000: " + (mTotalTime/1000));
 						mSubTime -= (mTotalTime/1000);
-						Log.d("DEBUGGERY", "mSubTime after change:" + mSubTime);
-						Log.d("DEBUGGERY", "mSubTime inside is: " + ((mTotalTime/1000) + mSubTime));
+//						Log.d("DEBUGGERY", "mSubTime after change:" + mSubTime);
+//						Log.d("DEBUGGERY", "mSubTime inside is: " + ((mTotalTime/1000) + mSubTime));
 					}
 				}
 			}
@@ -192,6 +211,8 @@ public class MainActivity extends Activity implements OnItemSelectedListener, On
 
             if (((mTotalTime/1000) + device.getTotal()) == 0 && device.getCountdown()) {
 				stopTimer();
+				//play ding.mp3
+				soundPool.play(soundMap.get(DING_SOUND_ID), 1, 1, 1, 0, 1f);
 				
 			} else {
 				mHandler.sendEmptyMessageDelayed(0, 250);
