e60d9ca435cb1e8529644a6481cb38969f7b8cac
myappleguy <myappleguy@gmail.com>
2013-03-14 20:53:06 -0500

    after getting MacMini reinstalled

--- baristaLog/src/me/toddpickell/baristalog/DataManager.java
+++ baristaLog/src/me/toddpickell/baristalog/DataManager.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.SyncStateContract.Constants;
 import android.util.Log;
 
 public class DataManager {
