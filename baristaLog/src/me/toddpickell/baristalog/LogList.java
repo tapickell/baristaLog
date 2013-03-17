7e4a7151fddd204ac42dad6ee0f8a91906a0e419
myappleguy <myappleguy@gmail.com>
2013-03-17 16:06:38 -0500

    got to build and run on device now need to add views for list to use

--- baristaLog/src/me/toddpickell/baristalog/LogList.java
+++ baristaLog/src/me/toddpickell/baristalog/LogList.java
@@ -20,90 +20,73 @@ public class LogList extends ListActivity {
 
 	private static final String ROW_ID = "row_id";
 	private ListView coffeeLogListView;
-	
-	//from manning example
+
+	// from manning example
+	private DataManager dataManager;
 	private LogAdapter logAdapter;
 	private List<LogNote> lognotes;
-	 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		coffeeLogListView = getListView();
-		coffeeLogListView.setOnItemClickListener(viewLogListener);
-		
-		//TODO map log field to textview in listview layout
-		String[] from = new String[] { "name" };
-//		int[] to = new int[] { R.id.coffeeLogTextView };
-//		logAdapter = new SimpleCursorAdapter(LogList.this, R.layout.coffeeLog_list_item, null, from, to);
-		
+
+		dataManager = new DataManager(this);
 		lognotes = new ArrayList<LogNote>();
-		
+		logAdapter = new LogAdapter(this, lognotes);
+
 		setListAdapter(logAdapter);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+		lognotes.clear();
+		lognotes.addAll(dataManager.getLogNoteHeaders());
+		logAdapter.notifyDataSetChanged();
 	}
 
-	@Override
-	protected void onStop() {
-		
-		Cursor cursor = logAdapter.getCursor();
-		if (cursor != null)
-			cursor.deactivate();
+	// private class GetLogTask extends AsyncTask<Object, Object, Cursor> {
+	//
+	// DatabaseConnector databaseConnector = new
+	// DatabaseConnector(LogList.this);
+	//
+	// @Override
+	// protected Cursor doInBackground(Object... params) {
+	// databaseConnector.open();
+	// return databaseConnector.getAllLogs();
+	// }
+	//
+	// @Override
+	// protected void onPostExecute(Cursor result) {
+	// logAdapter.changeCursor(result);
+	// databaseConnector.close();
+	// }
+	//
+	// }// end GetLogTask class
 
-		logAdapter.changeCursor(null);
-		super.onStop();
-	}
-	
-	private class GetLogTask extends AsyncTask<Object, Object, Cursor> {
-
-		DatabaseConnector databaseConnector = new DatabaseConnector(LogList.this);
-		
-		@Override
-		protected Cursor doInBackground(Object... params) {
-			databaseConnector.open();	
-			return databaseConnector.getAllLogs();
-		}
-
-		@Override
-		protected void onPostExecute(Cursor result) {
-			logAdapter.changeCursor(result);
-			databaseConnector.close();
-		}
-		
-	}// end GetLogTask class
-
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-//		return super.onCreateOptionsMenu(menu);
-//		MenuInflater inflater = getMenuInflater();
-//		inflater.inflate(R.menu.coffeelog_menu, menu);
+		// return super.onCreateOptionsMenu(menu);
+		// MenuInflater inflater = getMenuInflater();
+		// inflater.inflate(R.menu.coffeelog_menu, menu);
 		return true;
 	}
 
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Intent addNewCoffeeLog = new Intent(LogList.this, AddEditCoffeeLog.class);
+		Intent addNewCoffeeLog = new Intent(LogList.this,
+				AddEditCoffeeLog.class);
 		startActivity(addNewCoffeeLog);
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 
+	public void onListItemClick(ListView arg0, View arg1, int arg2, long arg3) {
+		Intent viewCoffeeLog = new Intent(LogList.this, ViewLog.class);
+		viewCoffeeLog.putExtra(ROW_ID, arg3);
+		startActivity(viewCoffeeLog);
 
-	OnItemClickListener viewLogListener = new OnItemClickListener() {
+	}
 
-		@Override
-		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-			Intent viewCoffeeLog = new Intent(LogList.this, ViewLog.class);
-			viewCoffeeLog.putExtra(ROW_ID, arg3);
-			startActivity(viewCoffeeLog);
-
-		}
-	};
-}//end CoffeeLog class
+}// end CoffeeLog class
